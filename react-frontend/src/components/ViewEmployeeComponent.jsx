1c4cc77413f6ee7e6b813f045300b4dfe1bbd5ca
vishal <vishalambekar459@gmail.com>
2022-07-16 01:28:21 +0530

    view page some issue

--- react-frontend/src/components/ViewEmployeeComponent.jsx
+++ react-frontend/src/components/ViewEmployeeComponent.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import EmployeeService from '../services/EmployeeService';
 
 class ViewEmployeeComponent extends Component {
 
@@ -6,15 +7,30 @@ class ViewEmployeeComponent extends Component {
         super(props)
 
         this.state = {
-
+            id: this.props.match.params.id,  //http://localhost:3000/view-employee/6  gets id from here
+            employee: {}
         }
     }
-     
 
+    componentDidMount(){
+         EmployeeService.getEmployeeById(this.setState.id).then( res => {
+            console.log("res :: "+res);
+            this.setState({employee: res.data});
+         })
+    }
+     
     render() {
         return (
             <div>
-                <h1>View employee</h1>
+                <div className = "card col-md-6 offset-md-3">
+                    <h3 className = "text-center"> View Employee Details</h3>
+                    <div className = "card-body">
+                        <div className = "row">
+                            <label>Employee First Name: </label>
+                            <div> { this.state.employee.firstName }</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
